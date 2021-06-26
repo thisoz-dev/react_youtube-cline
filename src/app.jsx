e8e2f95119686daeabb4fc3f3d556620023a1ebe
thisoz-dev <thisoz.dev@gmail.com>
2021-06-26 19:59:02 +0900

    back to the when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
@@ -11,20 +11,22 @@ function App({ youtube }) {
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
-  const search = (query) => {
+
+  const search = useCallback((query) => {
     youtube
       .search(query) //
       .then((videos) => {
         setVideos(videos);
         setSelectedVideo(null);
       });
-  };
+  }, []);
 
   useEffect(() => {
     youtube
       .mostPopular() //
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './search_header.module.css';
 
-const SearchHeader = ({ onSearch }) => {
+const SearchHeader = memo(({ onSearch }) => {
   const inputRef = useRef();
   const handleSearch = () => {
     const value = inputRef.current.value;
@@ -16,6 +16,7 @@ const SearchHeader = ({ onSearch }) => {
       handleSearch();
     }
   };
+  console.log('header');
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -34,6 +35,6 @@ const SearchHeader = ({ onSearch }) => {
       </button>
     </header>
   );
-};
+});
 
 export default SearchHeader;
